0e85a4ad91bd7ae91a6c3bb201b2cc16165e4c29
souly1515 <souly1515@gmail.com>
2015-06-11 00:35:16 +0800

    added all the animation states

--- FPY2/Assets/Scripts/Anim_controller.cs
+++ FPY2/Assets/Scripts/Anim_controller.cs
@@ -2,16 +2,27 @@
 using System.Collections;
 
 public class Anim_controller : MonoBehaviour {
-	public Sprite weapon1;
-	public Sprite weapon2;
-	public Sprite weapon3;
-	Animator anim;
+	public string spriteName;
+	SpriteRenderer sRenderer;
+	public GameObject weaponGO;
 	// Use this for initialization
 	void Start () {
-		anim = GetComponent<Animator> ();
+		sRenderer = weaponGO.GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void LateUpdate () {
+		Resources.LoadAll ("");
+		string curSprite = sRenderer.sprite.name;
+		if (weapon1.name == curSprite) {
+			sRenderer.sprite=weapon1;
+		}
+		else if (weapon2.name == curSprite) {
+			sRenderer.sprite=weapon2;
+		}
+		else if (weapon3.name == curSprite) {
+			sRenderer.sprite=weapon3;
+		}
+
 	}
 }
