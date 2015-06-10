2853ad1813e7cb9ec56318a610a785130c376271
souly1515 <130214X@M615G406.sidm.student.local>
2015-06-10 15:42:26 +0800

    added more animations

---
+++ FPY2/Assets/Scripts/Anim_controller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Anim_controller : MonoBehaviour {
+	public Sprite weapon1;
+	public Sprite weapon2;
+	public Sprite weapon3;
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
