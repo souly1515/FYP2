1af0702ce9c6cb695c5b9ecc4dd65dc0640ef3f0
souly1515 <souly1515@gmail.com>
2015-06-24 23:52:13 +0800

    did some stuff to the ignore file and done tadpole thing

---
+++ Assets/Scripts/Charger.cs
@@ -0,0 +1,161 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Charger :  E_Base {
+	Vector3 targ;//where its charging
+	public Vector3 charVel;
+	const float chargeCoolOff=0.50f;//after the attack miss how long it will charge for
+	const float idleTime=3.0f;
+	const float prepTime=1.0f;
+	Animator anim;
+	bool left=true;
+	public enum Internal_AttackState
+	{
+		CHARGE_PREP,
+		CHARGE,
+		CHARGE_SLOW,
+		IDLE
+	}
+	public Internal_AttackState a_State=Internal_AttackState.CHARGE_PREP;
+
+	override protected void Start ()
+	{
+		base.Start ();
+		base.detectionRange = 10;
+		base.attackRange = 5;
+		anim = gameObject.GetComponent<Animator> ();
+	}
+
+	protected override void Update ()
+	{
+		base.Update ();
+		if (charVel.x > 0) {
+			if (left) {
+				left = false;
+				Vector3 temp=transform.localScale;
+				temp.x*=-1;
+				transform.localScale = temp;	
+			}
+
+		} else {
+			if(!left)
+			{
+				left=true;
+				Vector3 temp=transform.localScale;
+				temp.x*=-1;
+				transform.localScale = temp;	
+			}
+		}
+	}
+
+	public override void Attack_State()
+	{
+		anim.SetFloat ("y_speed", charVel.y);
+		charVel.z = 0;
+		switch (a_State) {
+		case Internal_AttackState.IDLE:
+			if(timeLeft>0)
+			{
+				timeLeft-=Time.deltaTime;
+			}
+			else{
+				targ=Target.transform.position;
+				charVel=targ-gameObject.transform.position;
+				charVel=charVel.normalized*5.0f;
+				timeLeft=UnityEngine.Random.Range(prepTime*0.8f,prepTime*1.2f);
+				a_State=Internal_AttackState.CHARGE_PREP;
+				anim.SetFloat ("y_speed", charVel.y);
+				anim.SetBool("Charge_Prep",true);
+			}
+			//stop moving and start counting down then switch to charge enemy
+			break;
+		case Internal_AttackState.CHARGE:
+			if(Vector3.Dot(targ-gameObject.transform.position,charVel)>0){//not passed yet
+				Vector3 temp=gameObject.transform.position;
+				charVel+=charVel.normalized*1.2f*Time.deltaTime;
+				gameObject.transform.position=temp+charVel*Time.deltaTime;
+			}
+			else{//passed the charge pos
+				timeLeft=UnityEngine.Random.Range(chargeCoolOff*0.8f,chargeCoolOff*1.2f);
+				a_State=Internal_AttackState.CHARGE_SLOW;
+			}
+			//move at high speed towards the enemy
+			//after moving pass the enemy start a countdown before stopping
+			//use dot product between moving direction and the direction between enemy and original player pos>0 to determine if it passed the player
+			break;
+		case Internal_AttackState.CHARGE_SLOW:
+			timeLeft-=Time.deltaTime;
+			Vector3 pos=gameObject.transform.position;
+			gameObject.transform.position=pos+charVel*Time.deltaTime;
+			if(timeLeft<0)
+			{
+				charVel=Vector3.Lerp(charVel,Vector3.zero,Time.deltaTime*5);
+			}
+			else{
+				charVel+=charVel.normalized*1.2f*Time.deltaTime;
+			}
+			if(charVel.sqrMagnitude<0.01)
+			{
+				a_State=Internal_AttackState.IDLE;
+				timeLeft=UnityEngine.Random.Range(idleTime*0.8f,idleTime*1.2f);
+				anim.SetBool("Charge",false);
+				//dist=UnityEngine.Random.Range(2,4);
+			}
+			break;
+		case Internal_AttackState.CHARGE_PREP:
+			targ=Target.transform.position;
+			charVel=targ-gameObject.transform.position;
+			charVel=charVel.normalized*5.0f;
+			anim.SetFloat ("y_speed", charVel.y);
+			timeLeft-=Time.deltaTime;
+			if(timeLeft<=0)
+			{
+				a_State=Internal_AttackState.CHARGE;
+				anim.SetBool("Charge_Prep",false);
+				anim.SetBool("Charge",true);
+			}
+			break;
+		}
+		charVel.z = 0;
+
+	}
+
+	protected override void ChangeState ()
+	{
+		switch (states) {
+		case E_States.IDLE_MOVE:
+			Target=Physics2D.OverlapCircle(gameObject.transform.position,detectionRange,Tracks);
+			if(Target)
+			{
+				//the moment detection occurs the enemy will charge at the player
+				states=E_States.ATTACK;
+
+				//uncomment once done with circle test
+				//*
+				states=E_States.ATTACK;
+				targ=Target.transform.position;
+				charVel=targ-gameObject.transform.position;
+				charVel=charVel.normalized*5.0f;
+				a_State=Internal_AttackState.CHARGE;
+				stateChange=true;
+				//*/
+			}
+			break;
+		case E_States.ATTACK:
+			Target=Physics2D.OverlapCircle(gameObject.transform.position,detectionRange,Tracks);
+			if(!Target)
+			{
+				states=E_States.IDLE_MOVE;
+				stateChange=true;
+			}
+			break;
+		}
+	}
+
+	public override void Tracking_State ()
+	{
+
+	}
+
+
+}
---
+++ Assets/Scripts/E_Base.cs
@@ -0,0 +1,153 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class E_Stats
+{
+	public int health;
+	public float defense;
+	public float attack;
+	public float moveSpd;
+	public float atkSpd;
+	public float minAggro;
+	public float maxAggro;
+};
+
+public enum E_States
+{
+	STATE_NONE=0,
+	IDLE_MOVE,
+	IDLE_NOTMOVE,
+	TRACKING,
+	ATTACK,
+	STATE_TOTAL
+}
+
+public abstract class E_Base : MonoBehaviour {
+	public E_States states;
+	public E_Stats stats=new E_Stats();
+	public int currentHealth;
+	public float knockbackDrag;
+	public LayerMask Tracks;
+	public float detectionRange=10;
+	public float attackRange=5;
+	public Collider2D Target;
+	protected Vector2 originalPos;
+	protected Vector2 lastIdlePos;
+	protected Vector2 knockBackEffect;
+	protected Rigidbody2D rb ;
+	public float timeLeft;//for timings with state machine
+	protected bool stateChange;//tracks if state has changed
+	bool stunned=false;
+	float stunDur = 0.0f;
+
+	// Use this for initialization
+	virtual protected void Start () {
+		stats.health = 5;
+		originalPos = transform.position;
+		rb = gameObject.GetComponent<Rigidbody2D> ();
+		knockbackDrag = 0.8f;
+		states = E_States.IDLE_MOVE;
+		stateChange = false;
+	}
+
+	public abstract void Attack_State();
+
+	public abstract void Tracking_State();
+
+	// Update is called once per frame
+	virtual protected void Update () {
+		currentHealth = stats.health;
+		if (knockBackEffect.sqrMagnitude > 0) {
+
+			Vector2 minus=rb.velocity*(1-knockbackDrag);
+			rb.velocity=rb.velocity-minus;
+			knockBackEffect-=minus;
+			
+			if(rb.velocity.sqrMagnitude<0.1f)
+			{
+				knockBackEffect=Vector2.zero;
+				rb.velocity=Vector2.zero;
+			}
+			else if(knockBackEffect.sqrMagnitude<0.1f)
+				knockBackEffect=Vector2.zero;
+
+		}
+		if (!stunned) 
+		{
+			ChangeState ();
+		}
+		else 
+		{
+			states=E_States.IDLE_NOTMOVE;
+			stunDur-=Time.deltaTime;
+			if(stunDur<=0)
+			{
+				stunned=false;
+			}
+		}
+		UpdateStates ();
+	}
+
+	public void ApplyDamage(float attack,float stunDur)
+	{
+		//apply armor application
+		//stats.health -= (int)attack;
+		Debug.Log ("Damaged\n");
+		if (stats.health <= 0) {
+			gameObject.SetActive (false);
+
+		}
+	}
+	public void KnockBack(float amount,Vector2 Dir)
+	{
+		knockBackEffect = Dir * amount;
+		rb.velocity = Dir * amount;
+	}
+
+	protected virtual void ChangeState()
+	{
+			switch (states) {
+		case E_States.IDLE_NOTMOVE:
+			Target = Physics2D.OverlapCircle (gameObject.transform.position, detectionRange, Tracks);
+			if (Target) {
+				states = E_States.TRACKING;
+				stateChange = false;
+			}
+			break;
+		case E_States.ATTACK:
+			if (!Physics2D.OverlapCircle (gameObject.transform.position, attackRange, Tracks)) {
+				states = E_States.TRACKING;
+				stateChange = true;
+			}
+			break;
+		case E_States.TRACKING:
+			if (!Physics2D.OverlapCircle (gameObject.transform.position, detectionRange, Tracks)) {
+				states = E_States.IDLE_NOTMOVE;
+				stateChange = true;
+				//rb.velocity=knockBackEffect;
+			} else if (Physics2D.OverlapCircle (gameObject.transform.position, attackRange, Tracks)) {
+				states = E_States.ATTACK;
+				stateChange = true;
+				//rb.velocity=knockBackEffect;
+			}
+
+			break;
+		}
+	}
+	protected  virtual void UpdateStates()
+	{
+		switch (states) {
+			
+		case E_States.IDLE_NOTMOVE:
+			break;
+		case E_States.ATTACK:
+			//attacking code here
+			Attack_State();
+			break;
+		case E_States.TRACKING:
+			Tracking_State();
+			break;
+		}
+		stateChange=false;
+	}
+}
