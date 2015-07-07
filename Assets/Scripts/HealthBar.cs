18438913466526cbdcce770b009e9ffc78fe6a71
souly1515 <souly1515@gmail.com>
2015-07-07 10:47:05 +0800

    minor changes to level 5 to show boss health

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -4,14 +4,17 @@ using System.Collections;
 public class HealthBar : MonoBehaviour {
 	public float barDisplay = 0.0f;
 	public GameObject player;
+	public GameObject enemy;
 	public RectTransform r;
 	C_Base script;
+	E_Base E_script;
 
 	void Start()
 	{
 		if (player) {
 			script = player.GetComponent<C_Base> ();
-
+		} else if (enemy) {
+			E_script=enemy.GetComponent<E_Base>();
 		}
 		r = GetComponent<RectTransform> ();
 	}
@@ -20,11 +23,18 @@ public class HealthBar : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (script) {
-			float scale=script.stats.Health/300.0f;
+			float scale = script.stats.Health / 300.0f;
 
-			Vector3 temp=r.localScale;
-			temp.x=scale;
-			r.localScale=temp;
+			Vector3 temp = r.localScale;
+			temp.x = scale;
+			r.localScale = temp;
+		} else if (E_script) {
+			
+			float scale = E_script.stats.health / 100.0f;
+			
+			Vector3 temp = r.localScale;
+			temp.x = scale;
+			r.localScale = temp;
 		}
 	}
 }
